dedeb8eb44b18596a147030f499edf44033307e5
mehedihasan826 <mehedihasan826@gmail.com>
2021-09-11 11:28:35 +0600

    commit

--- List/Dictionary.py
+++ List/Dictionary.py
@@ -4,3 +4,15 @@ my_dict['Second'] = 'C++' #changing element
 print(my_dict)
 my_dict['Third'] = 'Ruby' #adding key-value pair
 print(my_dict)
+a = my_dict.pop('Third') #pop element
+print('Value:', a)
+print('Dictionary:', my_dict)
+b = my_dict.popitem() #pop the key-value pair
+print('Key, value pair:', b)
+print('Dictionary', my_dict)
+my_dict.clear() #empty dictionary
+print( my_dict)
+print(my_dict.keys()) #get keys
+print(my_dict.values()) #get values
+print(my_dict.items()) #get key-value pairs
+print(my_dict.get('First'))
